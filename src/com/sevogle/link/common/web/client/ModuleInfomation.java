4a583e6dbc52b5b15bfdfa6a2be3730b9df8678b
Adam Bovill <adam@sevogle.com>
2011-06-27 16:44:30 -0700

    InfoServlet has been added

---
+++ src/com/sevogle/link/common/web/client/ModuleInfomation.java
@@ -0,0 +1,62 @@
+package com.sevogle.link.common.web.client;
+
+import java.io.Serializable;
+
+public class ModuleInfomation implements Serializable {
+    private static final long serialVersionUID = 6913592724123084882L;
+
+    private int versionMajor;
+    private int versionMinor;
+    private int versionDot;
+    private String id;
+    private String name;
+    private String state;
+
+    public int getVersionMajor() {
+        return versionMajor;
+    }
+
+    public void setVersionMajor(int versionMajor) {
+        this.versionMajor = versionMajor;
+    }
+
+    public int getVersionMinor() {
+        return versionMinor;
+    }
+
+    public void setVersionMinor(int versionMinor) {
+        this.versionMinor = versionMinor;
+    }
+
+    public int getVersionDot() {
+        return versionDot;
+    }
+
+    public void setVersionDot(int versionDot) {
+        this.versionDot = versionDot;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
