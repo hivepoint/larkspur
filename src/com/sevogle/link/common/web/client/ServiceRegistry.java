2e569fc6f458d6e83e9025767c992ee46d8fc0ba
Preet <preet@sevogle.com>
2011-07-07 10:20:08 -0700

    added entry for twitter extension

--- src/com/sevogle/link/common/web/client/ServiceRegistry.java
+++ src/com/sevogle/link/common/web/client/ServiceRegistry.java
@@ -6,6 +6,10 @@ public interface ServiceRegistry {
 
     ExtensionType getGmailExtensionType();
 
+    ExtensionType getTwitterExtensionType();
+
+    ExtensionType[] getAllExtensionTypes();
+
     public static class ServiceRegistryImpl implements ServiceRegistry {
 
         private static ServiceRegistry instance = new ServiceRegistryImpl();
@@ -19,6 +23,9 @@ public interface ServiceRegistry {
                 "Google Mail", "Extension that handles gmails", "http://localhost:24001",
                 "18d85810-8651-4007-84ba-6f8d805e7d6f");
 
+        private final ExtensionType _twitterExtension = new ExtensionType("com.sevogle.extensions.twitter", "Twitter",
+                "Sevogle Twitter extension", "http://localhost:24002", "2aad2d7d-650f-4194-871e-40f23ca3dd4d");
+
         @Override
         public String getLinkBaseURL() {
             return "http://localhost:24000/";
@@ -27,6 +34,16 @@ public interface ServiceRegistry {
         @Override
         public ExtensionType getGmailExtensionType() {
             return _gmailExtension;
+        }
+
+        @Override
+        public ExtensionType getTwitterExtensionType() {
+            return _twitterExtension;
+        }
+
+        @Override
+        public ExtensionType[] getAllExtensionTypes() {
+            return new ExtensionType[] { _gmailExtension, _twitterExtension };
         }
 
     }
